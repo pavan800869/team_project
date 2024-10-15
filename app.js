6e9cee7a61ce0def4bcbc601c1df4a4391e6d0a7
Pavan Kumar <pavan.kumarmetla@gmail.com>
2024-10-15 11:12:42 +0530

    added .env

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use('/', hotelRoutes);
 app.use('/auth', authRoutes);
 
 
-const PORT = process.env.PORT || 1000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 module.exports = app;
